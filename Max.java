25b89cbaef1794406c1a9abdd576b4a7017da52d
Serhiy Presser <59319806+Serhiy13@users.noreply.github.com>
2020-03-19 23:35:36 +0200

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,18 @@
+package condition;
+
+public class Max {
+    public static int maxA(int left, int right) {
+        int result = right;
+        return result;
+    }
+    public static int maxB(int left, int right) {
+        int result = left;
+        return result;
+    }
+    public static int maxC(int left, int right) {
+        int result = left;
+        int result1 = right;
+        return result;
+    }
+
+}
