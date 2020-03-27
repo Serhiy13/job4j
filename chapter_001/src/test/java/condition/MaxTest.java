23886775282bc5e1c3cf828bf6f24640c9cce5cc
Serhiy Presser <svpresser@gmail.com>
2020-03-27 21:16:26 +0200

    MaxTest

--- chapter_001/src/test/java/condition/MaxTest.java
+++ chapter_001/src/test/java/condition/MaxTest.java
@@ -6,20 +6,20 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMaxA1To2Then2() {
-        int result = Max.maxA(1, 2);
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMaxB1To2Then2() {
-        int result = Max.maxB(2, 1);
+    public void whenMax2To1Then2() {
+        int result = Max.max(2, 1);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMaxC1To2Then2() {
-        int result = Max.maxC(2, 2);
+    public void whenMax2To2Then2() {
+        int result = Max.max(2, 2);
         assertThat(result, is(2));
     }
 }
