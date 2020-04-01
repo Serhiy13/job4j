c7c1c9b424be7c55804afb457d73aca200ae0dc0
Serhiy Presser <59319806+Serhiy13@users.noreply.github.com>
2020-04-01 19:07:11 +0300

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -2,7 +2,7 @@ package condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        boolean result = ab>(ac+bc);
+        boolean result = ab>(ac+bc) && ac>(ab+bc) && bc>(ab+ac);
         return false;
     }
 }
