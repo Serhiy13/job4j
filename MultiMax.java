7494a93cc19d7f294a0ea14fdf0ffc6b13ae4562
Serhiy Presser <59319806+Serhiy13@users.noreply.github.com>
2020-04-04 02:32:17 +0300

    Update MultiMax.java

--- MultiMax.java
+++ MultiMax.java
@@ -4,7 +4,7 @@ public class MultiMax {
     public int max(int first, int second, int third) {
         int result = first>=(second+third)?  first :(second+third):
                      second>=(first+third)? second : (first+third):
-                     third>=(first+third)? third : (first+third);
+                     third>=(first+second)? third : (first+second):
         return result;
 
 
